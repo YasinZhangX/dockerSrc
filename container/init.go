6e2101105e2ec13f49a0663ad787e8bf130ad77d
yasinZhang <zyxuan97@foxmail.com>
2021-05-25 22:30:30 +0800

    init mydocker

---
+++ container/init.go
@@ -0,0 +1,23 @@
+package container
+
+import (
+	"os"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
+
+func RunContainerInitProcess(command string, args []string) error {
+	logrus.Infof("command %s", command)
+
+	// 显示要求mount namespace独立
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	argv := []string{command}
+	if err := syscall.Exec(command, argv, os.Environ()); err != nil {
+		logrus.Errorf(err.Error())
+	}
+
+	return nil
+}
